4c476fd1f807291f5488ed1577cefc2668f50d5e
NFT-CID <nft.cid@gmail.com>
2024-06-28 08:27:16 +0200

    changed: About page, test should fail

--- pages/home/about.tsx
+++ pages/home/about.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function About() {
   return (
     <div>
-      <h1>About</h1>
+      <h1>About Me!</h1>
       <Link href="/home">Home</Link>
     </div>
   );
